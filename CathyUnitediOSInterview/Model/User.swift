f0ac12870a95c82a914c8ad03347d39051302ccf
Krauser Huang <crescentdence@gmail.com>
2025-06-08 12:17:37 +0800

    Create User structure

---
+++ CathyUnitediOSInterview/Model/User.swift
@@ -0,0 +1,28 @@
+//
+//  User.swift
+//  CathyUnitediOSInterview
+//
+//  Created by Tai Chin Huang on 2025/6/8.
+//
+
+import Foundation
+
+struct UserResponse: Codable {
+    let response: [User]
+}
+
+struct User: Codable {
+    let name: String?   // 姓名
+    let kokoid: String? // kokoID
+}
+
+/*
+ {
+   "response": [
+     {
+       "name": "蔡國泰",
+       "kokoid": "Mike"
+     }
+   ]
+ }
+ */
